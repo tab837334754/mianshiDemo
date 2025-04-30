de41294587b0b4059b1df69e0a0f0b789183facd
tangbo <837334754@qq.com>
2025-04-30 11:37:44 +0800

    github pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    base: '/',
+    base: '/mianshiDemo/',
     plugins: [vue()],
     define: {
       'process.env': {
